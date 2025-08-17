8e32d0568c75b6a8862675c27b96f77a98fff0fb
sakab257 <slmbouskine93@gmail.com>
2025-08-17 03:06:37 +0200

    Ajout de certains articles -> Il en reste 4...

--- components/articles.tsx
+++ components/articles.tsx
@@ -24,10 +24,10 @@ export default function Articles({ selectedTag }: ArticlesProps) {
     useEffect(() => {
         const loadArticles = async () => {
             try {
-                const articles = await fetchArticles(); // Utiliser ça au taff pour drupal pcq a la maison j'ai pas la db
-                setAllArticles(articles);
+                // const articles = await fetchArticles(); // Utiliser ça au taff pour drupal pcq a la maison j'ai pas la db
+                // setAllArticles(articles);
+                setAllArticles(query.data.testgraphqlvuesGraphql1.results);
             } catch (error) {
-                console.log("Utilisation des données statiques car l'API n'est pas disponible");
                 setAllArticles(query.data.testgraphqlvuesGraphql1.results);
             } finally {
                 setLoading(false);
@@ -84,17 +84,17 @@ export default function Articles({ selectedTag }: ArticlesProps) {
               className={`flex w-full h-80 gap-6 pb-2 items-stretch ${index % 2 !== 0 ? 'flex-row-reverse' : ''}`}
             >
               {article.image && (
-                <div className="flex-shrink-0 w-1/5 h-full">
+                <div className="w-1/6 h-full">
                   <Image
                       src={`${article.image}`} // mettre ${article.image quand pas acces au back drupal} http://localhost:8084
                     alt={article.title}
                     width={500}
                     height={300}
-                    className="h-full w-full object-cover border-3 border-black"
+                    className="h-full object-cover border-3 border-black"
                   />
                 </div>
               )}
-              <div className={`flex flex-col gap-4 p-6 border-3 border-black w-4/5 h-full justify-start`}>
+              <div className={`flex flex-col gap-4 p-6 border-3 border-black w-full h-full justify-start`}>
                 {article.tags && (
                   <div className="border-b-2 border-black pb-2">
                     <p className="uppercase text-sm font-semibold">
@@ -103,7 +103,7 @@ export default function Articles({ selectedTag }: ArticlesProps) {
                   </div>
                 )}
                 <div className="flex-1 flex flex-col items-center justify-evenly">
-                  <h2 className={`text-3xl font-bold leading-tight uppercase text-center ${newsreader.className}`}>
+                  <h2 className={`text-2xl font-bold leading-tight uppercase text-center ${newsreader.className}`}>
                     {article.title}
                   </h2>
                   {article.body && (
